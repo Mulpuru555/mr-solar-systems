b8bd7e0949c39c40be841519603ad249832e38fa
Mulpuru jashwanth <84584485+Mulpuru555@users.noreply.github.com>
2026-03-18 21:39:59 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,32 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBNtPFzFkYLpbv8vgfeQ0_uE42JT7h28bc",
-  authDomain: "mr-solar-portal.firebaseapp.com",
-  projectId: "mr-solar-portal",
-  storageBucket: "mr-solar-portal.firebasestorage.app",
-  messagingSenderId: "1017116122935",
-  appId: "1:1017116122935:web:db1256b90c66c96ad644b2"
+
+apiKey: "AIzaSyBNtPFzFkYLpbv8vgfeQ0_uE42JT7h28bc",
+
+authDomain: "mr-solar-portal.firebaseapp.com",
+
+projectId: "mr-solar-portal",
+
+storageBucket: "mr-solar-portal.firebasestorage.app",
+
+messagingSenderId: "1017116122935",
+
+appId: "1:1017116122935:web:db1256b90c66c96ad644b2"
+
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
+
+export const storage = getStorage(app);
